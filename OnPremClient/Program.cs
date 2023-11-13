1f211e25bd8ac8d2cd6ce85ce9fd84b26ca462fe
Remco Brosky <remcobrosky@microsoft.com>
2023-11-13 14:04:48 +0100

    Added SQL

--- OnPremClient/Program.cs
+++ OnPremClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Client;
 
@@ -13,6 +14,10 @@ const string CERT_SUBJECT = "selfsigned-demo";
 string clientId = config["ClientId"]; //ClientId of the app registration
 string tenantId = config["TenantId"]; //Tenant of the EntraId Tenant
 string apiScope = config["ApiScope"]; //Scope of the API -> "api://<clientid of the api>/.default"
+string dbScope = "https://database.windows.net//.default";
+string sqlEndpoint = config["SqlEndpoint"]; //SQL Endpoint
+string sqlDatabase = config["SqlDatabase"]; //SQL Database
+
 
 
 X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
@@ -44,17 +49,19 @@ IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create
     .WithCertificate(cert)
     .Build();
 
-var result = await app.AcquireTokenForClient(
+var apiTokenResult = await app.AcquireTokenForClient(
             new[] { apiScope })
             .ExecuteAsync();
 
-Console.WriteLine("Acquired this JWT");
+
+Console.WriteLine();
+Console.WriteLine("Acquired this JWT for API");
 Console.WriteLine("----------------------------");
-Console.WriteLine(result.AccessToken);
+Console.WriteLine(apiTokenResult.AccessToken);
 
 
 HttpClient client = new HttpClient();
-client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiTokenResult.AccessToken);
 var response = await client.GetAsync("https://localhost:7074/WeatherForecast");
 
 //Give the API some time to start up
@@ -64,4 +71,55 @@ Console.WriteLine("Response from API");
 Console.WriteLine("----------------------------");
 Console.WriteLine($"STATUS: {response.StatusCode}");
 Console.WriteLine(await response.Content.ReadAsStringAsync());
-Console.ReadLine();
+
+
+
+//Connect to SQL Database on Azure with token
+//ServicePrincipal needs permissions on the database
+var dbTokenResult = await app.AcquireTokenForClient(
+            new[] { dbScope })
+            .ExecuteAsync();
+
+Console.WriteLine();
+Console.WriteLine("Acquired this JWT for SQL");
+Console.WriteLine("----------------------------");
+Console.WriteLine(dbTokenResult.AccessToken);
+
+
+// Define the retry logic parameters - when using SQL Serverless and database is asleep
+var sqlRetryLogicOption = new SqlRetryLogicOption()
+{
+    // Tries 5 times before throwing an exception
+    NumberOfTries = 5,
+    // Preferred gap time to delay before retry
+    DeltaTime = TimeSpan.FromSeconds(5),
+    // Maximum gap time for each delay time before retry
+    MaxTimeInterval = TimeSpan.FromSeconds(20)
+};
+
+// Create a retry logic provider
+SqlRetryLogicBaseProvider provider = SqlConfigurableRetryFactory.CreateExponentialRetryProvider(sqlRetryLogicOption);
+
+SqlConnection conn = new SqlConnection($"Data Source={sqlEndpoint}; Initial Catalog={sqlDatabase}");
+conn.RetryLogicProvider = provider;
+conn.AccessToken = dbTokenResult.AccessToken;
+
+
+Console.WriteLine();
+Console.WriteLine("Response from SQL");
+Console.WriteLine("----------------------------");
+try
+{
+    conn.Open();
+    Console.WriteLine("Connected to SQL Database");
+    conn.Close();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+Console.WriteLine();
+Console.WriteLine("Hit any key to close...");
+Console.ReadKey();
